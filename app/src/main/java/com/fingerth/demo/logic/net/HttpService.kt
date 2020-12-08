1d802f5ac0063594b8407f71d01ffed68f16a500
fingerth <zhou1094277148@gmail.com>
2020-12-08 15:45:33 +0800

    Update HttpService.kt

--- app/src/main/java/com/fingerth/demo/logic/net/HttpService.kt
+++ app/src/main/java/com/fingerth/demo/logic/net/HttpService.kt
@@ -1,11 +1,14 @@
-package com.fingerth.sunnyweather.logic.network
+package com.fingerth.demo.logic.net
 
 import com.fingerth.demo.logic.model.JokePicBean
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.QueryMap
+import retrofit2.http.*
 
 interface HttpService {
     @GET("joke/")
     fun getJokePics(@QueryMap map: Map<String, String>): Call<JokePicBean>
+
+    @FormUrlEncoded
+    @POST("loginV2/m_confirm")
+    fun onLogin(@FieldMap map: Map<String, String>): Call<String>
 }
