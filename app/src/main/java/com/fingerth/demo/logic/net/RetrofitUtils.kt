6609ed143ea6a5283d12b660a2a6456adbebd42c
fingerth <zhou1094277148@gmail.com>
2020-12-08 15:44:49 +0800

    Update RetrofitUtils.kt

--- app/src/main/java/com/fingerth/demo/logic/net/RetrofitUtils.kt
+++ app/src/main/java/com/fingerth/demo/logic/net/RetrofitUtils.kt
@@ -1,4 +1,4 @@
-package com.fingerth.sunnyweather.logic.network
+package com.fingerth.demo.logic.net
 
 import android.util.Log
 import okhttp3.OkHttpClient
@@ -14,17 +14,34 @@ import java.util.concurrent.TimeUnit
 object RetrofitUtils {
     private const val DEFAULT_TIMEOUT = 120L
     private const val TAG = "RetrofitUtils"
-    private const val BASE_URL = "https://bird.ioliu.cn/"
     private const val hasTag = true
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(StringConverterFactory.create())
-        .addConverterFactory(GsonConverterFactory.create())
-        .client(getOkHttpClient())
-        .build()
 
-    fun <T> creat(serviceClass: Class<T>): T = retrofit.create(serviceClass)
-    inline fun <reified T> creat(): T = creat(T::class.java)
+    private val builder: Retrofit.Builder
+        get() {
+            return Retrofit.Builder()
+                .addConverterFactory(StringConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okClient)
+        }
+
+    private val okClient: OkHttpClient by lazy {
+        val loggingInterceptor = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger { message ->
+            if (!hasTag) return@Logger
+            Log.d(TAG, "OkHttp====Message: $message")
+        })
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        OkHttpClient.Builder() //设置超时
+            .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    fun <T> retrofit(baseUrl: String, serviceClass: Class<T>): T = builder.baseUrl(baseUrl).build().create(serviceClass)
+
+//    fun <T> creat(serviceClass: Class<T>): T = retrofit.create(serviceClass)
+//    inline fun <reified T> creat(): T = creat(T::class.java)
 
 
     private class StringConverter : Converter<ResponseBody, String> {
@@ -38,32 +55,10 @@ object RetrofitUtils {
         }
     }
 
-    private fun getOkHttpClient(): OkHttpClient {
-        val loggingInterceptor =
-            HttpLoggingInterceptor(HttpLoggingInterceptor.Logger { message ->
-                if (!hasTag) return@Logger
-                Log.d(TAG, "OkHttp====Message: $message")
-            })
-        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-        return OkHttpClient.Builder() //设置超时
-            .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
-            .writeTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
-            .readTimeout(
-                DEFAULT_TIMEOUT,
-                TimeUnit.SECONDS
-            )
-            .addInterceptor(loggingInterceptor)
-            .build()
-
-    }
 
     private class StringConverterFactory : Converter.Factory() {
         // 我们只关实现从ResponseBody 到 String 的转换，所以其它方法可不覆盖
-        override fun responseBodyConverter(
-            type: Type,
-            annotations: Array<Annotation>,
-            retrofit: Retrofit
-        ): Converter<ResponseBody, *>? {
+        override fun responseBodyConverter(type: Type, annotations: Array<Annotation>, retrofit: Retrofit): Converter<ResponseBody, *>? {
             return if (type === String::class.java) {
                 StringConverter.INSTANCE
             } else null
